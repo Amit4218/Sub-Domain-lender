26d1908d9751c9e09e8af8722f9d635202d625ee
Amit4218 <amitbhagat621@gmail.com>
2026-01-21 16:32:53 +0530

    feat: added a verify_email function for user email verification

--- app/auth/user_auth.py
+++ app/auth/user_auth.py
@@ -1,5 +1,11 @@
+import uuid
+
+from sqlalchemy import select
+
 from app.models import User
 from config import db
+from utils import BASE_URL
+from utils.email_sender import send_email
 
 
 def login_user(email: str, password: str):
@@ -31,10 +37,46 @@ def register_user(email: str, password: str):
     new_user = User(email=email)
     new_user.hash_password(password=password)
 
+    data = {"id": new_user.id, "email": new_user.email}
+
+    user_token = uuid.uuid3(uuid.NAMESPACE_DNS, new_user.email)
+
+    final_url = f"{BASE_URL}?token={user_token}&email={email}"
+
+    sended_email = send_email(
+        user_email=new_user.email,
+        user_name=new_user.email.split("@")[0],
+        token=final_url,
+    )
+
+    if not sended_email["id"]:
+        return None, "Error registering, please try again.."
+
     db.session.add(new_user)
     db.session.commit()
     db.session.refresh(new_user)
 
-    data = {"id": new_user.id, "email": new_user.email}
+    return data, None
+
+
+def verify_user_token(token, email):
+    is_token_valid = uuid.uuid3(uuid.NAMESPACE_DNS, email)
+
+    if not is_token_valid:
+        return None, "Invalid token! please register again."
+
+    user = db.session.execute(
+        select(User).where(User.email == email)
+    ).scalar_one_or_none()
+
+    if not user:
+        return None, "User not found! please register again."
+
+    user.is_email_verified = True
+
+    db.session.commit()
+    db.session.refresh(user)
+
+    data = {"id": user.id, "email": user.email}
 
     return data, None

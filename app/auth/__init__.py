87dea6bbdda1d1c5f6ff40acb28bb729455bc248
Amit4218 <amitbhagat621@gmail.com>
2026-02-01 22:15:42 +0530

    added functions to verify user and update pasword

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,15 @@
-from .user_auth import login_user, register_user, verify_user_token
+from .user_auth import (
+    login_user,
+    register_user,
+    verify_user_and_send_email,
+    verify_user_and_update_password,
+    verify_user_token,
+)
 
-__all__ = ["login_user", "register_user", "verify_user_token"]
+__all__ = [
+    "login_user",
+    "register_user",
+    "verify_user_token",
+    "verify_user_and_send_email",
+    "verify_user_and_update_password",
+]
--- app/auth/user_auth.py
+++ app/auth/user_auth.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 from app.models import User
 from config import db
 from utils import BASE_URL
-from utils.email_sender import send_email
+from utils.email_sender import send_email, send_reset_password_email
 
 
 def login_user(email: str, password: str):
@@ -41,7 +41,7 @@ def register_user(email: str, password: str):
 
     user_token = uuid.uuid3(uuid.NAMESPACE_DNS, new_user.email)
 
-    final_url = f"{BASE_URL}?token={user_token}&email={email}"
+    final_url = f"{BASE_URL}/verify?token={user_token}&email={email}"
 
     sended_email = send_email(
         user_email=new_user.email,
@@ -80,3 +80,54 @@ def verify_user_token(token, email):
     data = {"id": user.id, "email": user.email}
 
     return data, None
+
+
+def verify_user_and_send_email(email):
+    if not email:
+        return None, "Email is required!"
+
+    user = db.session.execute(
+        select(User).where(User.email == email)
+    ).scalar_one_or_none()
+
+    if not user:
+        return None, "User not found! please check your email!"
+
+    user_token = uuid.uuid3(uuid.NAMESPACE_DNS, str(user.id))
+
+    final_url = f"{BASE_URL}/reset_password?token={user_token}&id={user.id}"
+
+    sended_email = send_reset_password_email(
+        user_email=email, user_name=email.split("@")[0], token=final_url
+    )
+
+    if not sended_email["id"]:
+        return None, "Error sending Email, please try again.."
+
+    return True, None
+
+
+def verify_user_and_update_password(token, user_id, new_password):
+    if not token or not user_id:
+        return None, "Something went wrong! please try again!"
+
+    if not new_password:
+        return None, "No password provoded!"
+
+    verify_token = str(uuid.uuid3(uuid.NAMESPACE_DNS, user_id))
+
+    if token != verify_token:
+        return None, "Something went wrong! please try again!"
+
+    user = db.session.execute(
+        select(User).where(User.id == uuid.UUID(user_id))
+    ).scalar_one_or_none()
+
+    if not user:
+        return None, "Something went wrong! please try again!"
+
+    user.hash_password(new_password)
+
+    db.session.commit()
+
+    return True, None

2b042afb2fa756f8b502d486eb41f1cee0ad9f7c
Amit4218 <amitbhagat621@gmail.com>
2026-02-01 22:12:35 +0530

    feat: added send_reset_password_email function to send reset password links

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -45,3 +45,43 @@ def send_email(user_email: str, user_name: str, token: str):
     email = resend.Emails.send(params)  # ty:ignore[invalid-argument-type]
 
     return email
+
+
+def send_reset_password_email(user_email: str, user_name: str, token: str):
+    html_structure = f"""
+    <html>
+    <head>
+        <style>
+            body {{ font-family: Arial, sans-serif; background-color: #f5f5f5; }}
+            .container {{ max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }}
+            h1 {{ color: #333; margin-bottom: 10px; }}
+            p {{ color: #666; font-size: 16px; }}
+            .content {{ margin: 20px 0; line-height: 1.6; }}
+            .verification-box {{ background-color: #f0f7ff; border-left: 4px solid #0066cc; padding: 15px; margin: 20px 0; }}
+            .btn {{ display: inline-block; background-color: black; color: #FFFFFF; padding: 12px 30px; text-decoration: none; border-radius: 5px; margin-top: 15px; }}
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <p>Hi {user_name} here is your Password reset link</p>
+                <div class="verification-box">
+                    <strong>Reset you'r password by clicking the link below:</strong><br>
+                    <a href="{token}" class="btn">Reset Password</a>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+    """
+
+    params = {
+        "from": "SubDomainLender <noreply@amit4218.fun>",
+        "to": user_email,
+        "subject": "Password Reset Link",
+        "html": html_structure,
+    }
+
+    email = resend.Emails.send(params)  # ty:ignore[invalid-argument-type]
+
+    return email

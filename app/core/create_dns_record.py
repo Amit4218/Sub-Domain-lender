1e4c0a6bc16aae739fbbd6e05479a613b72d4925
Amit4218 <amitbhagat621@gmail.com>
2026-02-03 16:00:51 +0530

    now returns an error if the subdomain is taken

--- app/core/create_dns_record.py
+++ app/core/create_dns_record.py
@@ -3,6 +3,7 @@ import requests
 from app.models import Record
 from config import db
 from utils import CLOUDFLARE_API_TOKEN, CLOUDFLARE_BASE_URL
+from utils.check_if_subdomain_available import check_if_subdomain_available
 
 ALLOWED_TYPES: list[str] = ["A", "AAAA", "CNAME", "MX"]
 
@@ -11,6 +12,13 @@ def create_record(
     id, type: str, name: str, content: str, ttl: int = 3600
 ) -> str | None:
     """Sends an post request to set an dns record and adds record to the db"""
+
+    # check if the sub-domain is taken
+
+    is_taken = check_if_subdomain_available(name=name)
+
+    if is_taken:
+        return f"{name} is already taken!"
 
     if type not in ALLOWED_TYPES:
         return f"Error! Can't set record: {type}"
